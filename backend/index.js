b9e41af0e98c7752ad805ead7b663cf95c46e38a
Karan Navani <kknavani@gmail.com>
2020-04-05 16:45:13 +0100

    added cron capability

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const cron = require("node-cron");
+const express = require("express");
+const { checkAmazonPrimeNow } = require("./amazon-prime-now");
+
+app = express();
+
+// schedule tasks to be run on the server
+cron.schedule("*/10 * * * *", function() {
+  checkAmazonPrimeNow();
+});
+
+app.listen(3128);
