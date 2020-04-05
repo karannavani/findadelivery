e2365fbd9f706450e9ccfe990d10438e9a9753ad
Karan Navani <kknavani@gmail.com>
2020-04-05 16:45:13 +0100

    added cron capability

--- backend/amazon-prime-now.js
+++ backend/amazon-prime-now.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const merchantId = "A3L2WCBX4NBSPG";
+const merchantId = "A2SQ1LUW1J67MC"; //needs to be checked and changed every time there is a new cart
 const ref = "pn_sc_ptc_bwr";
 const url =
   `https://primenow.amazon.co.uk/checkout/enter-checkout?merchantId=${merchantId}&ref=${ref}`;
 
 
+const checkAmazonPrimeNow = () => {
 axios({
   method: "get",
   url: url,
@@ -18,6 +19,15 @@ axios({
     const html = response.data;
     const $ = cheerio.load(html);
     const deliverySlot = $("#delivery-slot-form > div > span").text();
-    console.log(deliverySlot);
+    console.log(
+      !deliverySlot.includes(
+        "No delivery windows for today or tomorrow are currently available. New delivery windows are released throughout the day."
+      )
+    );
   })
   .catch(console.error);
+}
+
+module.exports = {
+  checkAmazonPrimeNow
+};
