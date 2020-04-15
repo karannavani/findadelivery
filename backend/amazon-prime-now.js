689609d9e100522764118c8a7c8acfffdb071a93
Karan Navani <kknavani@gmail.com>
2020-04-15 21:36:10 +0100

    added experimental api code in index.js

--- backend/amazon-prime-now.js
+++ backend/amazon-prime-now.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const sgMail = require("@sendgrid/mail");
 
-const merchantId = "A2SQ1LUW1J67MC"; //needs to be checked and changed every time there is a new cart
+const merchantId = "A3L2WCBX4NBSPG"; //needs to be checked and changed every time there is a new cart
 const ref = "pn_sc_ptc_bwr";
 const url = `https://primenow.amazon.co.uk/checkout/enter-checkout?merchantId=${merchantId}&ref=${ref}`;
 
@@ -17,11 +17,11 @@ const msg = {
   to: process.env.PERSONAL_EMAIL,
   from: "checkoutapp@example.com",
   subject: "AMAZON PRIME DELIVERY SLOT AVAILABLE",
-  text: `Go to checkout - ${url}`
+  text: `Go to checkout - ${url}`,
 };
 
 const slotAvailabilityArray = [];
-const verificationArray = [];
+let verificationArray = [];
 let deliverySlot;
 let checkFalseAlarm = false;
 let verificationCron;
@@ -32,19 +32,23 @@ const checkAmazonPrimeNow = () => {
     method: "get",
     url: url,
     headers: {
-      Cookie: process.env.AMAZON_PRIME_NOW_COOKIE
-    }
+      Cookie: process.env.AMAZON_PRIME_NOW_COOKIE,
+    },
   })
-    .then(response => {
+    .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
+      deliveryHeader = $(
+        "#delivery-slot > div > div > div > div.a-section.a-spacing-none.pn-panel-heading-wrapper > div > h2"
+      ).text();
       deliverySlot = $("#delivery-slot-form > div > span").text();
+      console.log(deliveryHeader);
       checkForAvailability(deliverySlot);
     })
     .catch(console.error);
 };
 
-const checkForAvailability = deliverySlot => {
+const checkForAvailability = (deliverySlot) => {
   deliverySlot.includes(
     "No delivery windows for today or tomorrow are currently available. New delivery windows are released throughout the day."
   )
@@ -56,7 +60,7 @@ const sendEmail = () => {
   sgMail.send(msg);
 };
 
-const compareToPrevious = boolean => {
+const compareToPrevious = (boolean) => {
   if (checkFalseAlarm) {
     console.log("verifying to prev");
     verificationArray.push(boolean);
@@ -78,15 +82,18 @@ const verifyIfFalseAlarm = () => {
   console.log("verification array looks like", verificationArray);
   let availabilityVerified;
   if (verificationArray.length <= 5) {
-    verificationArray.filter(availability => {
+    verificationArray.filter((availability) => {
       if (!availability) {
         checkFalseAlarm = false;
+        verificationArray = [];
         console.log("stopping function");
         verificationCron.stop();
         return;
       } else if (availability && verificationArray.length === 4) {
         availabilityVerified = true;
         checkFalseAlarm = false;
+        verificationArray = [];
+
         console.log("stopping function");
         verificationCron.stop();
         return;
@@ -101,19 +108,19 @@ const verifyIfFalseAlarm = () => {
 
 const runVerificationCron = () => {
   checkFalseAlarm = true;
-  verificationCron = cron.schedule("* * * * * *", function() {
+  verificationCron = cron.schedule("* * * * * *", function () {
     console.log("starting from cron a");
     checkAmazonPrimeNow();
   });
 };
 
 // schedule tasks to be run on the server
-cron.schedule("*/30 * * * * *", function() {
+cron.schedule("* * * * *", function () {
   checkAmazonPrimeNow();
 });
 
 app.listen(3125);
 
 module.exports = {
-  checkAmazonPrimeNow
+  checkAmazonPrimeNow,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,59 @@
 const cron = require("node-cron");
 const express = require("express");
-const { checkAmazonPrimeNow } = require("./amazon-prime-now");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+// const { checkAmazonPrimeNow } = require("./amazon-prime-now");
 
-app = express();
+const app = express();
 
-// schedule tasks to be run on the server
-cron.schedule("*/5 * * * * *", function() {
-  checkAmazonPrimeNow();
+const ads = [{ title: "Hello, world (again)!" }];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan("combined"));
+
+// defining an endpoint to return all ads
+app.get("/", (req, res) => {
+  res.send(ads);
+  console.log("sup1");
+
+  cron.schedule("* * * * * *", () => {
+    console.log('sup');
+  })
 });
 
-app.listen(3128);
+// // schedule tasks to be run on the server
+// cron.schedule("* * * * * *", function() {
+//   // const item = fetchQueue();
+//   createCron(fetchQueue());
+// });
+
+
+// const fetchQueue = () => {
+// // fetching what's in the queue
+// return {
+//   id: 123,
+//   email: "kknavani@gmail.com",
+//   grocery: "Amazon"
+// }
+// }
+
+// const createCron = (item) => {
+// item.cron = cron.schedule("*/3 * * * * *", function() {
+//   console.log('hello');
+// })
+// }
+
+app.listen(3124, () => {
+  console.log("listening on port 3124");
+});
