75e9d9b3dea3ea0c95c219472a78ed460bcd14d0
Karan Navani <kknavani@gmail.com>
2020-04-21 23:31:10 +0100

    committing unstaged files

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const admin = require("firebase-admin");
+const serviceAccount = require("./checkout-app-uk-firebase-adminsdk-2zjvs-54e313e107.json"); // this is generated from the Service accounts tab in Firebase, the file is hidden for security reasons
 // const { checkAmazonPrimeNow } = require("./amazon-prime-now");
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://checkout-app-uk.firebaseio.com",
+});
+const db = admin.firestore();
 const app = express();
-
-const ads = [{ title: "Hello, world (again)!" }];
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -22,22 +27,35 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan("combined"));
 
-// defining an endpoint to return all ads
-app.get("/", (req, res) => {
-  res.send(ads);
-  console.log("sup1");
-
-  cron.schedule("* * * * * *", () => {
-    console.log('sup');
-  })
+app.get("/api/trigger-job-check", (req, res) => {
+    res.status(200).send({
+      success: "true",
+      message: "Checking the database",
+    });
+    getJobs();
 });
+
+const getJobs = () => {
+  db.collection("jobs")
+    .get()
+    .then((jobs) => {
+      jobs.forEach(job => {
+        if (job.data().state === 'Scheduled') {
+          console.log(job.data().state === "Scheduled");
+          console.log('jobs that are scheduled', job.data())
+        } 
+        })
+    })
+    .catch((err) => {
+      console.log("Error getting jobs", err);
+    });
+};
 
 // // schedule tasks to be run on the server
 // cron.schedule("* * * * * *", function() {
 //   // const item = fetchQueue();
 //   createCron(fetchQueue());
 // });
-
 
 // const fetchQueue = () => {
 // // fetching what's in the queue
