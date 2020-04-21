af481325318989a6866cd9b3b89d488f39fd28ce
Karan Navani <kknavani@gmail.com>
2020-04-21 23:30:21 +0100

    backend is now informed when tasks are scheduled from the frontend

--- checkout-app/src/app/shared/services/scheduling/scheduling.service.ts
+++ checkout-app/src/app/shared/services/scheduling/scheduling.service.ts
@@ -1,28 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SchedulingService {
-  constructor(private firestore: AngularFirestore) {}
+  constructor(private firestore: AngularFirestore, private http: HttpClient, private authService: AuthenticationService) {}
 
   createJob() {
-
     const created = new Date().toISOString();
+    const userId = this.authService.getUserId();
     const data = {
+      userId,
       created,
-      service: 'Amazon',
+      store: 'Amazon',
+      type: 'Delivery',
+      state: 'Scheduled'
     };
     return new Promise<any>((resolve, reject) => {
       this.firestore
         .collection('jobs')
         .add(data)
         .then(
-          // add API call here to get the backend to check the queue
-          (res) => {},
+          (res) => {
+            this.triggerJobCheck();
+          },
           (err) => reject(err)
         );
     });
+  }
+
+  triggerJobCheck() {
+    console.log('triggered');
+    this.http.get('http://localhost:3124/api/trigger-job-check').subscribe();
   }
 }
