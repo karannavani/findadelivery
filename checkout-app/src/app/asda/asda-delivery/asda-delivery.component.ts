13ba1f66c24747ccd1e7376f3b719f66e43b54bf
Karan Navani <kknavani@gmail.com>
2020-04-26 19:40:54 +0100

    user postcode is now saved to the database

--- checkout-app/src/app/asda/asda-delivery/asda-delivery.component.ts
+++ checkout-app/src/app/asda/asda-delivery/asda-delivery.component.ts
@@ -3,6 +3,7 @@ import { SchedulingService } from 'src/app/shared/services/scheduling/scheduling
 import { FormControl } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthenticationService } from 'src/app/shared/services/authentication/authentication.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-asda-delivery',
@@ -17,22 +18,61 @@ export class AsdaDeliveryComponent implements OnInit {
   ) {}
 
   postcode = new FormControl('');
+  userId = this.authenticationService.getUserId();
+  user = null;
 
   ngOnInit(): void {
-    // this.populatePostcode();
+    this.checkIfUserExists();
   }
 
   populatePostcode() {
-    const userId = this.authenticationService.getUserId();
-
     return this.firestore
-      .collection('users', (ref) => ref.where('userId', '==', userId))
+      .collection('users', (ref) => ref.where('userId', '==', this.userId))
       .snapshotChanges()
       .subscribe((res) => console.log('res is', res));
   }
 
   scheduleJob(store) {
-    this.schedulingService.createJob(store);
+    if (this.user) {
+      this.updatePostCode();
+    } else {
+      this.firestore
+        .collection('users')
+        .add({ userId: this.userId, postcode: this.postcode.value });
+    }
+    // this.schedulingService.createJob(store);
     console.log('created job');
+  }
+
+  checkIfUserExists() {
+    this.firestore
+      .collection('users', (ref) => ref.where('userId', '==', this.userId))
+      .snapshotChanges()
+      .subscribe((res) => {
+        if (res.length !== 0) {
+          this.populatePostcode();
+          this.user = res[0];
+          console.log('user exists');
+        }
+        console.log('no user exists');
+      });
+  }
+
+  updatePostCode() {
+    if (this.user.postcode !== this.postcode.value) {
+      return this.firestore
+        .collection('users', (ref) => ref.where('userId', '==', this.userId))
+        .snapshotChanges()
+        .pipe(
+          map((users) =>
+            users.map((user) => {
+              return user.payload.doc.id;
+            })
+          )
+        )
+        .subscribe((id) => {
+          this.firestore.doc(`users/${id}`).update({ postcode: this.postcode.value });
+        });
+    }
   }
 }
