63b696cf54b9bb76332e2c754defc9843ec69159
Karan Navani <kknavani@gmail.com>
2020-05-05 19:05:39 +0100

    Asda is now its own page

--- checkout-app/src/app/app-routing.module.ts
+++ checkout-app/src/app/app-routing.module.ts
@@ -4,13 +4,23 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { LoginRedirectGuard } from './shared/guards/login-redirect.guard';
+import { AsdaDeliveryComponent } from './asda/asda-delivery/asda-delivery.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [LoginRedirectGuard] },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoginRedirectGuard],
+  },
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'asda',
+    component: AsdaDeliveryComponent,
     canActivate: [AuthGuard],
   },
 ];
