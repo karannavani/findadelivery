d99b911899d4a2da8f9120f9189d517a32e996a2
Karan Navani <kknavani@gmail.com>
2020-05-05 21:03:55 +0100

    adding firebase config to prod

--- checkout-app/src/environments/environment.prod.ts
+++ checkout-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAjed4rP5NP5-hkPRd_GTtcrsld_1tAlsE',
+    authDomain: 'checkout-app-uk.firebaseapp.com',
+    databaseURL: 'https://checkout-app-uk.firebaseio.com',
+    projectId: 'checkout-app-uk',
+    storageBucket: 'checkout-app-uk.appspot.com',
+    messagingSenderId: '829833888493',
+    appId: '1:829833888493:web:d3530a947789cc99bb02e4',
+  },
 };
