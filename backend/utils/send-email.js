777345d34410555e9761884890000eebd3a1eb08
kanelincoln <kane.lincoln@icloud.com>
2020-05-19 17:19:54 +0100

    Started pulling text from external JSON file

--- backend/utils/send-email.js
+++ backend/utils/send-email.js
@@ -2,30 +2,49 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const constructMessage = (vendor) => {
+const insertDynamicContent = (origContent, timeSlot) => {
+  // This whole thing could be much more sophisticated and robust but it
+  // should do for now, though it will modify the current email format slightly.
+
+  // TODO: This could be extracted into a separate function(?)
+  const slotDate = timeSlot[0].toDateString();
+  const startTime = `${timeSlot[0].getUTCHours()}:${timeSlot[0].getUTCMinutes()}`;
+  const endTime = `${timeSlot[1].getUTCHours()}:${timeSlot[1].getUTCMinutes()}`;
+
+  console.log('origContent is:', origContent);
+  console.log('timeSlot is:', timeSlot);
+  
+  const contentToInsert = `on ${slotDate} at ${startTime}-${endTime}. `;
+  const newContent = {
+    subject: origContent.subject,
+    body: origContent.body[1] + contentToInsert + origContent.body[2]
+  };
+
+  console.log('newContent is:', newContent);
+  
+  return true; // TODO: Delete line
+  return newContent;
+};
+
+const constructMessage = (vendor, timeSlot) => {
   // This will probably become far more complex when you/we start dispatching
   // high-quality HTML emails but for now only exists to return the relevant
   // text for each vendor.
 
-  switch(vendor) {
-    case 'ASDA':
-      return ('This is Asda');
+  const contentMappings = require('../data/email-text.json');
+  const vendors = Object.keys(contentMappings);
+  if (!vendor || !vendors.includes(vendor)) return false;
 
-    case 'AMAZON':
-      return('This is Amazon');
-
-    default:
-      return false;
-  }
+  return (timeSlot ? insertDynamicContent(contentMappings[vendor], details) : contentMappings[vendor]);
 };
 
-const sendEmail = (vendor, recipients = []) => {
+const sendEmail = (vendor, addresses = [], timeSlot) => {
+  const primaryAddress = process.env.PERSONAL_EMAIL;
   const sender = 'checkoutapp@example.com'; 
   const subject = "Find a Delivery - We've found a slot!";
-  const recipient = process.env.PERSONAL_EMAIL;
 
-  if (recipient) recipients.push(recipient);
-  if (!recipient && recipients.length === 0) return { statusCode: 400, message: 'No recipient(s) found.' };
+  if (primaryAddress) addresses.push(primaryAddress);
+  if (!addresses || !primaryAddress && addresses.length === 0) return { statusCode: 400, message: 'No addresses(s) found.' };
 
   const text = constructMessage(vendor);
   if (!text) return { statusCode: 400, message: 'No valid vendor found.' };
--- backend/utils/send-email.test.js
+++ backend/utils/send-email.test.js
@@ -15,7 +15,7 @@ describe('sendEmail()', () => {
     });
 
     test('Returns a 400 Bad Request if no vendor is passed', async () => {
-      const response = await sendEmail();
+      const response = await sendEmail(null);
       expect(response.statusCode).toBe(400);
     });
 
@@ -27,31 +27,32 @@ describe('sendEmail()', () => {
 
 
   describe('process.env.PERSONAL_EMAIL is undefined', () => {
-    test('Returns a 400 Bad Request if no recipient is found', async () => {
-      const response = await sendEmail();
+    test('Returns a 400 Bad Request if no email address is found', async () => {
+      const vendor = 'ASDA';
+      const response = await sendEmail(vendor, null);
 
       expect(response.statusCode).toBe(400);
     });
     
-    test('Returns a 400 Bad Request if recipient is found but no vendor is passed', async () => {
-      const recipients = [
+    test('Returns a 400 Bad Request if email address is found but no vendor is passed', async () => {
+      const addresses = [
         'nice_guy_curtis@test.com',
         'nicer_guy_karan@test.com'
       ];
 
-      const response = await sendEmail(null, recipients);
+      const response = await sendEmail(null, addresses);
 
       expect(response.statusCode).toBe(400);
     });
 
-    test('Returns a 200 OK if recipient is found', async () => {
+    test('Returns a 200 OK if address is found', async () => {
       const vendor = 'ASDA';
-      const recipients = [
+      const addresses = [
         'nice_guy_curtis@test.com',
         'nicer_guy_karan@test.com'
       ];
 
-      const response = await sendEmail(vendor, recipients);
+      const response = await sendEmail(vendor, addresses);
 
       expect(response.statusCode).toBe(200);
     });
