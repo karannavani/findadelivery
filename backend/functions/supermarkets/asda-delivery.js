66f19e5caf5574ac133088c2a82161d6178f49de
Curtis Burns <curtis.burns28@gmail.com>
2020-05-23 22:30:21 +0100

    Little refactor (#6)
    * Little refactor
    
    Haven't actually had a chance to test this out, but let me know your comments.
    
    * Fixed instead of commenting changes... Tested as best I could.
    
    * Oops. Forgot to take this out.
    
    * added dynamic date to asda, updated node version and added support for firebase emulator
    
    * updated git ignore
    
    * updated git ignore
    
    * adding a bogus change
    
    * adding comment for availability toggle
    
    * adding comment
    
    Co-authored-by: kanelincoln <kane.lincoln@icloud.com>
    Co-authored-by: Karan Navani <kknavani@gmail.com>

--- backend/functions/supermarkets/asda-delivery.js
+++ backend/functions/supermarkets/asda-delivery.js
@@ -12,7 +12,6 @@ const db = admin
     "Asda Delivery"
   )
   .firestore();
-// const db = admin.firestore();
 
 sgMail.setApiKey(functions.config().sendgrid.api_key);
 
@@ -24,94 +23,94 @@ class AsdaDelivery {
     this.availabilityVerified = false;
     this.checkAsda(postcode);
   }
-  checkAsda(postcode) {
-    console.log("running asda", postcode);
-    axios
-      .post("https://groceries.asda.com/api/v3/slot/view", {
-        requestorigin: "gi",
-        data: {
-          service_info: {
-            fulfillment_type: "DELIVERY",
-            enable_express: false,
-          },
-          start_date: "2020-04-25T00:00:00+01:00",
-          end_date: "2020-05-15T00:00:00+01:00",
-          reserved_slot_id: "",
-          service_address: {
-            postcode,
-          },
-          customer_info: {
-            account_id: "8774316050",
-          },
-          order_info: {
-            order_id: "20027220446",
-            restricted_item_types: [],
-            volume: 0,
-            weight: 0,
-            sub_total_amount: 0,
-            line_item_count: 0,
-            total_quantity: 0,
-          },
-        },
-      })
-      .then((res) => {
-        console.log(`statusCode: ${res.status_code}`);
-        this.getSlots(res.data.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
 
-  async getSlots(data) {
-    if (data.slot_days) {
-      console.log("get slots called");
-      data.slot_days.forEach((day) => {
-        day.slots.forEach((slot) => {
-          if (
-            slot.slot_info.status !== "UNAVAILABLE" &&
-            !this.availabilityVerified
-          ) {
-            console.log(slot.slot_info.status);
-            const startTime = new Date(slot.slot_info.start_time);
-            const endTime = new Date(slot.slot_info.end_time);
-            this.availabilityVerified = true;
-            this.sendEmail(startTime, endTime).then(async () => {
-            return db.doc(`jobs/${this.docId}`)
-              .update({
-                state: "Completed",
-              })
-              .then(() => {
-                // there is a slot open on date:
-                console.log("date", `${startTime.toDateString()}`);
-                console.log(
-                  "start time",
-                  `${startTime.getUTCHours()}:${startTime.getUTCMinutes()}0`
-                );
-                console.log(
-                  "start time",
-                  `${endTime.getUTCHours()}:${endTime.getUTCMinutes()}0`
-                );
-              });
-            })
-          }
-        });
-      });
+  async checkAsda(postcode) {
+    console.log("running asda", postcode);
+    const start_date = new Date();
+    let end_date = new Date(start_date);
+    // setting end date to be one month from the current date
+    end_date.setMonth(start_date.getMonth() + 1);
+    // adding the ASDA request headers
+    const requestorigin = "gi";
+    const data = {
+      service_info: {
+        fulfillment_type: "DELIVERY",
+        enable_express: false,
+      },
+      start_date,
+      end_date,
+      reserved_slot_id: "",
+      service_address: {
+        postcode,
+      },
+      customer_info: {
+        account_id: "8774316050",
+      },
+      order_info: {
+        order_id: "20027220446",
+        restricted_item_types: [],
+        volume: 0,
+        weight: 0,
+        sub_total_amount: 0,
+        line_item_count: 0,
+        total_quantity: 0,
+      },
+    };
+
+    try {
+      console.log("Making call...");
+      const res = await axios.post(
+        "https://groceries.asda.com/api/v3/slot/view",
+        {
+          requestorigin,
+          data,
+        }
+      );
+      const slotData = res.data.data;
+      this.getSlots(slotData);
+    } catch (error) {
+      console.log({ error });
     }
   }
 
-  sendEmail(startTime, endTime) {
+  async getSlots({ slot_days }) {
+    // If there are no slot days, do nothing.
+    console.log("get slots called with");
+    if (!slot_days) return;
+
+    // Otherwise...
+    slot_days.forEach((day) =>
+      day.slots.forEach(async (slot) => {
+        if (
+          slot.slot_info.status !== "UNAVAILABLE" &&
+          !this.availabilityVerified
+        ) {
+          const startTime = new Date(slot.slot_info.start_time);
+          const endTime = new Date(slot.slot_info.end_time);
+          this.availabilityVerified = true; // this is the toggle that prevents more slots from being sent as separate emails
+
+          await this.sendEmail(startTime, endTime);
+
+          // Update doc with new state
+          await db.doc(`jobs/${this.docId}`).update({ state: "Completed" });
+        }
+      })
+    );
+  }
+
+  async sendEmail(startTime, endTime) {
     const msg = {
       to: this.email,
       from: "noreply@findadelivery.com",
       subject: "ASDA DELIVERY SLOT AVAILABLE",
       text: `A delivery slot has become available for ${startTime.toDateString()}, ${startTime.getUTCHours()}:${startTime.getUTCMinutes()}0 - ${endTime.getUTCHours()}:${endTime.getUTCMinutes()}0
-    
+
     Book your slot - https://groceries.asda.com/checkout/book-slot?tab=deliver&origin=/`,
     };
-
     console.log("sending email");
-    return sgMail.send(msg);
+    await sgMail.send(msg);
+
+    return;
   }
 }
 
