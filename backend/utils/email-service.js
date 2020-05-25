648119c62ae0c207cf97cfacc34ab0847d15df86
kanelincoln <kane.lincoln@icloud.com>
2020-05-25 23:05:23 +0100

    Deleted some unnecessary stuff

--- backend/utils/email-service.js
+++ backend/utils/email-service.js
@@ -20,7 +20,6 @@ const formatSlotData = (slots, maxSlotsPerEmail) => {
 
     formattedSlots.push(formattedSlot);
   }
-
   return formattedSlots;
 }
 
@@ -32,12 +31,10 @@ const buildSgPersonalization = (merchant, slots, address, url) => {
     merchant: supermarkets[merchant],
     slots: formatSlotData(slots, maxSlotsPerEmail)
   };
-
   const personalization = {
     to: [{ email: address }],
     dynamic_template_data: dynamicTemplateData
   };
-
   // console.log('personalization is:', personalization);
   return personalization;
 };
@@ -48,14 +45,11 @@ const buildSgPayload = (merchant, slots, addresses, url) => {
     template_id:'d-ae627fe97d3c43209c1608fb43dfe7f0',
     personalizations: addresses.map((address) => buildSgPersonalization(merchant, slots, address, url)) // SendGrid only lets you create 1000 of these per email
   };
-
   // console.log('sgPayload is:', sgPayload);
   return sgPayload;
 }
 
 const defineAddresses = (addresses) => {
-  console.log('Looking for addresses...');
-
   const primaryAddress = process.env.PERSONAL_EMAIL;
 
   if (!primaryAddress && !addresses) throw { statusCode: 400, message: 'No recipient(s) found.' };
@@ -69,31 +63,24 @@ const defineAddresses = (addresses) => {
 };
 
 const sendEmail = async (data) => {
+  // TODO: Build logger for debugging/testing
   try {
     if (!data) throw { statusCode: 400, message: 'No data passed.' };
-    // TODO: Let's quit immediately if we can't find a valid API key. It's fine for
-    // now but is a good check to have in place.
-    // if (process.env.SENDGRID_API_KEY) sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    // else throw { statusCode: 400, message: 'No API key found.' };
 
-    console.log('Checking required parameters exist...');
-
+    console.log('Checking parameters...');
     const requiredArgs = ['merchant', 'slots', 'url'];
     requiredArgs.forEach((arg) => {
-      // TODO: Check a supported merchant has been submitted
-      // TODO: Build logger for debugging/testing
       // console.log(`Checking data[${arg}] contains data...`);
       if (data[arg] === undefined || data[arg] === null) throw { statusCode: 400, message: 'Required argument missing: ' + arg }
     });
 
-    const { merchant, slots, url } = data;
-
     console.log('Finished checking parameters.');
+    const { merchant, slots, url } = data;
+    console.log('Looking for addresses...');
     const addresses = defineAddresses(data.addresses);
     console.log('Addresses found:', addresses);
 
     const sgPayload = buildSgPayload(merchant, slots, addresses, url);
-
     await axios({
       method: 'post',
       url: 'https://api.sendgrid.com/v3/mail/send',
