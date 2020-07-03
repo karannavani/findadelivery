989df9e3346e38ecf94e1d79d79c3b2fc57c8d1f
Karan Navani <kknavani@gmail.com>
2020-07-03 23:54:34 +0100

    updated flow with rxjs

--- checkout-app/src/app/request-access/request-access-nhs/request-access-nhs.component.ts
+++ checkout-app/src/app/request-access/request-access-nhs/request-access-nhs.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Subscription, of, Observable, Subject } from 'rxjs';
+import {
+  Subscription,
+  of,
+  Observable,
+  Subject,
+  forkJoin,
+  merge,
+  concat,
+} from 'rxjs';
 import { RequestAccessService } from '../service/request-access.service';
+import { tap, map, switchMap, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-request-access-nhs',
@@ -19,24 +28,72 @@ export class RequestAccessNhsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
-  onSubmit(): void {
+  onSubmit(): any {
     if (this.email.valid && this.email.value.endsWith('nhs.uk')) {
-      this.subscriptions.add(
+      if (!this.verifyEmail(this.email.value)) {
         this.requestAccessService
-          .checkIfEmailExists(this.email.value, 'nhsSignups')
-          .subscribe((emailExists) => {
-            console.log('email exists is', emailExists);
-            if (emailExists) {
-            } else {
-              this.error = null;
-              this.submitted = true;
-              this.requestAccessService.completeSignup(
-                this.email.value,
-                'nhsSignups'
-              );
-            }
-          })
-      );
+          .completeSignup(this.email.value, 'nhsSignups')
+          .subscribe((res) => {
+            console.log('res is', res);
+          });
+      } else {
+        this.error = 'This email already exists';
+      }
+      // this.requestAccessService
+      //   .checkIfEmailExists(this.email.value, 'nhsSignups')
+      //   .pipe()
+      //   // .pipe(
+      //   //   tap((emailExists) => console.log('emailExists is', emailExists)),
+      //   //   map((emailExists) => {
+      //   //     if (!emailExists) {
+      //   //       return forkJoin(
+      //   //         this.requestAccessService.completeSignup(
+      //   //           this.email.value,
+      //   //           'nhsSignUps'
+      //   //         )
+      //   //       );
+      //   //     } else {
+      //   //       console.error('email already exists');
+      //   //       return 'hi';
+      //   //     }
+      //   //   })
+      //   //   // tap((res) => console.log('tap is', res)),
+      //   //   // map((signupCompleted) => {
+      //   //   //   if (signupCompleted) {
+      //   //   //     return signupCompleted.
+      //   //   //   } else {
+      //   //   //     this.error = 'This email already exists';
+      //   //   //   }
+      //   //   // })
+      //   // )
+      //   .subscribe((res) => console.log('res is', res));
+
+      // this.subscriptions.add(
+      //   this.requestAccessService
+      //     .checkIfEmailExists(this.email.value, 'nhsSignups')
+      //     .subscribe((emailExists) => {
+      //       if (!emailExists) {
+      //         console.log('email does not exist');
+      //         this.requestAccessService
+      //           .completeSignup(this.email.value, 'nhsSignups')
+      //           .pipe(
+      //             tap((signupCompleted) => {
+      //               if (signupCompleted) {
+      //                 this.error = null;
+      //                 this.submitted = true;
+      //               } else {
+      //                 this.error =
+      //                   'Unable to complete your signup at the moment, please try again later';
+      //                 this.submitted = false;
+      //               }
+      //             })
+      //           );
+      //       } else {
+      //         this.error = 'This email already exists';
+      //         console.error('email already exists');
+      //       }
+      //     })
+      // );
     } else if (this.email.invalid) {
       this.error = 'Please enter a valid email address';
     } else if (!this.email.value.endsWith('nhs.uk')) {
@@ -45,6 +102,16 @@ export class RequestAccessNhsComponent implements OnInit, OnDestroy {
       this.error = 'Please enter a valid email address';
     }
   }
+
+  verifyEmail(email: string): any {
+    this.requestAccessService
+      .checkIfEmailExists(email, 'nhsSignups')
+      .pipe(map((emailExists) => emailExists));
+  }
+
+  // completeSignup(): any {
+  //   this.requestAccessService.completeSignup()
+  // }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
--- checkout-app/src/app/request-access/service/request-access.service.ts
+++ checkout-app/src/app/request-access/service/request-access.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subscription, Subject, Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { filter } from 'rxjs/operators';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { filter, map } from 'rxjs/operators';
 import { generate } from 'voucher-code-generator';
 
 @Injectable({
@@ -9,50 +9,104 @@ import { generate } from 'voucher-code-generator';
 })
 export class RequestAccessService {
   constructor(private firestore: AngularFirestore) {}
-  subscriptions = new Subscription();
 
   checkIfEmailExists(email: string, collection: string): Observable<boolean> {
     const emailExists = new Subject<boolean>();
 
-    this.subscriptions.add(
-      this.firestore
-        .collection(collection)
-        .get()
-        .subscribe((res) => {
-          if (!res.empty) {
-            console.log('res is', res.docs);
-            console.log(res.docs.filter((doc) => doc.data().email === email));
-            res.docs.filter((doc) => doc.data().email === email).length
-              ? emailExists.next(true)
-              : emailExists.next(false);
-          } else {
-            emailExists.next(false);
-          }
-        })
-    );
+    this.firestore
+      .collection(collection)
+      .get()
+      .subscribe((res) => {
+        if (!res.empty) {
+          res.docs.filter((doc) => doc.data().email === email).length
+            ? emailExists.next(true)
+            : emailExists.next(false);
+        } else {
+          emailExists.next(false);
+        }
+      });
     return emailExists.asObservable();
   }
 
-  completeSignup(email: string, collection: string): void {
-    const created = new Date().toISOString();
-    const invite = '';
-    const data = { created, email, invite };
-    this.generateInvite();
-    // this.firestore.collection(collection).add(data);
+  completeSignup(email: string, collection: string): Observable<boolean> {
+    const signupCompleted = new Subject<boolean>();
+    console.log('complete sign up called');
+
+    this.createInvite(email).subscribe((inviteCreated) => {
+      if (inviteCreated) {
+        console.log('invite created in compl sign up');
+        const created = new Date().toISOString();
+        const invite = '';
+        const data = { created, email, invite };
+        // this.firestore.collection(collection).add(data);
+        signupCompleted.next(true);
+      } else {
+        console.error('unable to complete sign up');
+        signupCompleted.next(false);
+      }
+    });
+
+    return signupCompleted.asObservable();
   }
 
-  generateInvite(): void {
-    // generate({
-    //   length: 5,
-    //   count: 1
-    // });
-    console.log(
-      generate({
-        length: 5,
-        count: 1,
-      })
-    );
-    let inviteCode, registered, sendInvite;
-    const data = { inviteCode, registered, sendInvite };
+  createInvite(email): Observable<boolean> {
+    console.log('create invite called');
+
+    const inviteCreated = new Subject<boolean>();
+
+    const inviteCode = generate({
+      length: 5,
+      count: 1,
+    })[0];
+
+    const created = new Date().toISOString();
+
+    this.checkIfInviteCodeExists(inviteCode).subscribe((inviteExists) => {
+      console.log('invite exists is', inviteExists);
+      if (!inviteExists) {
+        const data = {
+          inviteCode,
+          registered: false,
+          sendInvite: false,
+          created,
+        };
+        try {
+          this.firestore
+            .collection('invites')
+            .add(data)
+            .then((ref) => console.log('ref is', ref)); // try passing ref in the invite created
+          inviteCreated.next(true);
+        } catch (error) {
+          console.error(error);
+          inviteCreated.next(false);
+        }
+      } else {
+        console.error('invite code already exists');
+        this.createInvite(email);
+      }
+    });
+
+    return inviteCreated.asObservable();
+  }
+
+  checkIfInviteCodeExists(inviteCode): Observable<boolean> {
+    console.log('called with', inviteCode);
+    const inviteExists = new Subject<boolean>();
+
+    this.firestore
+      .collection('invites')
+      .get()
+      .subscribe((res) => {
+        if (!res.empty) {
+          console.log('inside check invite');
+          res.docs.filter((doc) => doc.data().inviteCode === inviteCode).length
+            ? inviteExists.next(true)
+            : inviteExists.next(false);
+        } else {
+          inviteExists.next(false);
+        }
+      });
+
+    return inviteExists.asObservable();
   }
 }
