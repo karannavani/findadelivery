078b6e994aafe6a300cb3cf46f64e1c919bbaa63
Karan Navani <kknavani@gmail.com>
2020-07-05 23:10:34 +0100

    Feature/login policing (#23)
    * added login policing
    
    * added forced log out
    
    * adding unsub on destroy

--- checkout-app/src/app/login/login.component.ts
+++ checkout-app/src/app/login/login.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from 'src/app/shared/services/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  subscriptions = new Subscription();
+  error = null;
 
-  constructor(public authService: AuthenticationService) {}
+  constructor(public authService: AuthenticationService) {
+    this.subscriptions.add(
+      this.authService
+        .getRegisterError()
+        .subscribe((error) => (this.error = error))
+    );
+  }
 
   ngOnInit(): void {}
 
   googleLogin() {
     this.authService.GoogleAuth();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
